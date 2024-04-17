981237002fb823b7f013b658c9e2e0fc32f83936
Josehpequeno <hicarojbs21@gmail.com>
2024-04-17 15:28:23 -0300

    fix password on workflow

--- tests/database.test.ts
+++ tests/database.test.ts
@@ -1,15 +1,11 @@
 import 'dotenv/config';
 import { AppDataSource } from "../src/utils/database";
 
-
-afterAll(async () =>{
-	await AppDataSource.close();
-})
-
 describe("Teste de conexão com o banco de dados", () => {
 	it("deve estabelecer uma conexão com o banco de dados", async() =>{
 		try {
 			await AppDataSource.initialize();
+			await AppDataSource.close();
 			expect(true).toBeTruthy();
 		} catch (error) {
 			throw new Error(`Erro ao conectar ao banco de dados: ${error}`);
--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -11,22 +11,9 @@ const FILE_PATH_2 = path.resolve(__dirname, '../data_2.txt');
 
 describe('Teste para upload', () => {
 	it("Deve retornar 200 ok ao enviar um arquivo via rota /upload", async () => {
-		const formData = new FormData();
-		formData.append('file', fs.createReadStream(FILE_PATH_1));
-		
-		const response = await axios.post(`http://localhost:${PORT}/upload`,
-		formData, {
-			headers: {
-		        'Content-Type': 'multipart/form-data',
-		    },
-		});
-
-		expect(response.status).toBe(200);
-	});
-
-	it("Deve retornar 200 ok ao enviar um arquivo via rota /upload", async () => {
+		try {
 			const formData = new FormData();
-			formData.append('file', fs.createReadStream(FILE_PATH_2));
+			formData.append('file', fs.createReadStream(FILE_PATH_1));
 			
 			const response = await axios.post(`http://localhost:${PORT}/upload`,
 			formData, {
@@ -34,15 +21,40 @@ describe('Teste para upload', () => {
 			        'Content-Type': 'multipart/form-data',
 			    },
 			});
-	
+
 			expect(response.status).toBe(200);
+		} catch (error) {
+			expect(error).toBe(null);	
+		}
+	});
+
+	it("Deve retornar 200 ok ao enviar um arquivo via rota /upload", async () => {
+			try {
+				const formData = new FormData();
+				formData.append('file', fs.createReadStream(FILE_PATH_2));
+				
+				const response = await axios.post(`http://localhost:${PORT}/upload`,
+				formData, {
+					headers: {
+				        'Content-Type': 'multipart/form-data',
+				    },
+				});
+
+				expect(response.status).toBe(200);
+			} catch (error) {
+				expect(error).toBe(null);	
+			}
 		});
 })
 
 describe("Teste para getData", () => {
 	it("Deve retornar 200 OK ao acessar a rota /getData", async() => {
-		const response = await axios.get(`http://localhost:${PORT}/getData`);
-		expect(response.status).toBe(200);
-		expect(response.data.result).toEqual('ok');
+		try {
+			const response = await axios.get(`http://localhost:${PORT}/getData`);
+			expect(response.status).toBe(200);
+			expect(response.data.result).toEqual('ok');
+		} catch (error) {
+			expect(error).toBe(null);	
+		}
 	})
 })
